1ad4c4a650e45453b2d545294cea4719af44968b
ministar <48432289+cdcpp@users.noreply.github.com>
2023-12-27 21:36:27 +0900

    [Bronze II] Title: 단어의 개수, Time: 268 ms, Memory: 19980 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        System.out.println(st.countTokens());
+    }//main
+}
